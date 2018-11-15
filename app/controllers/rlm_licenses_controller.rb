f719eab562191f52604685e747769cf9339bbeaa
Florian Eck <it-support@friends-systems.de>
2018-11-15 10:26:25 +0100

    Remove layout

--- app/controllers/rlm_licenses_controller.rb
+++ app/controllers/rlm_licenses_controller.rb
@@ -22,7 +22,7 @@ class RlmLicensesController < ApplicationController
   def get_lefs_json
     @result = ::LefService.issue_lefs_as_jsonp(params)
     #response.headers['Access-Control-Allow-Origin'] = '*'
-    render template: "rlm/get_lefs_json"
+    render template: "rlm/get_lefs_json", layout: nil
   end
   
   def invoice_licenses
